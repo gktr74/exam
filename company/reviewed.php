0e11d4c726cba121a0a9f256410f13ac51c9c271
srv176941 <srv176941@c17m.hoster.ru>
2022-05-17 10:50:01 +0300

    2

---
+++ company/reviewed.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("test", "Тестовое значение");
+$APPLICATION->SetPageProperty("title", "Отзывы");
+$APPLICATION->SetPageProperty("keywords", "Отзывы, компания, мебель");
+$APPLICATION->SetPageProperty("description", "Отзывы о компании");
+$APPLICATION->SetTitle("Отзывы");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

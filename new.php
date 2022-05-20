bea20b4410f7fedb954c030a61241d50145ca6d3
yakovlev_a <gktr74@mail.ru>
2022-05-20 13:17:09 +0500

    final 20.05

--- new.php
+++ new.php
@@ -2,20 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?>
-<?$APPLICATION->IncludeComponent(
-    "bitrix:menu",
-    "left_menu",
-    Array(
-        "ALLOW_MULTI_SELECT" => "N",
-        "CHILD_MENU_TYPE" => "left",
-        "DELAY" => "N",
-        "MAX_LEVEL" => "2",
-        "MENU_CACHE_GET_VARS" => array(""),
-        "MENU_CACHE_TIME" => "3600",
-        "MENU_CACHE_TYPE" => "N",
-        "MENU_CACHE_USE_GROUPS" => "Y",
-        "ROOT_MENU_TYPE" => "left",
-        "USE_EXT" => "N"
-    )
-);?><br>
+<?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "nav", Array(
+    "PATH" => "",	// Путь, для которого будет построена навигационная цепочка (по умолчанию, текущий путь)
+    "SITE_ID" => "s1",	// Cайт (устанавливается в случае многосайтовой версии, когда DOCUMENT_ROOT у сайтов разный)
+    "START_FROM" => "0",	// Номер пункта, начиная с которого будет построена навигационная цепочка
+),
+    false
+);?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

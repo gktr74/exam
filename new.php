9995e25f9269b1108fbf69658375f1943329944a
yakovlev_a <gktr74@mail.ru>
2022-05-20 13:25:22 +0500

    final 20.05

--- new.php
+++ new.php
@@ -2,11 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?>
-<?$APPLICATION->IncludeComponent("bitrix:breadcrumb", "nav", Array(
-    "PATH" => "",	// Путь, для которого будет построена навигационная цепочка (по умолчанию, текущий путь)
-    "SITE_ID" => "s1",	// Cайт (устанавливается в случае многосайтовой версии, когда DOCUMENT_ROOT у сайтов разный)
-    "START_FROM" => "0",	// Номер пункта, начиная с которого будет построена навигационная цепочка
-),
-    false
-);?>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

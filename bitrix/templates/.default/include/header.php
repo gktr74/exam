21f6d0b9368e3d992cdd6dcb92e06649d23d0914
yakovlev_a <gktr74@mail.ru>
2022-05-20 09:45:16 +0500

    20.05all

--- bitrix/templates/.default/include/header.php
+++ bitrix/templates/.default/include/header.php
@@ -62,18 +62,18 @@
         </tr>
     </table>
     <?$APPLICATION->IncludeComponent("bitrix:menu", "top_multi", Array(
-        "ALLOW_MULTI_SELECT" => "N",
-        "CHILD_MENU_TYPE" => "left",
-        "DELAY" => "N",
-        "MAX_LEVEL" => "2",
-        "MENU_CACHE_GET_VARS" => array(
+        "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+        "CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+        "DELAY" => "N",	// Откладывать выполнение шаблона меню
+        "MAX_LEVEL" => "2",	// Уровень вложенности меню
+        "MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
             0 => "",
         ),
-        "MENU_CACHE_TIME" => "3600",
-        "MENU_CACHE_TYPE" => "N",
-        "MENU_CACHE_USE_GROUPS" => "Y",
-        "ROOT_MENU_TYPE" => "top",
-        "USE_EXT" => "N",
+        "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+        "MENU_CACHE_TYPE" => "N",	// Тип кеширования
+        "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+        "ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+        "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
     ),
         false
     );?>
--- new.php
+++ new.php
@@ -2,4 +2,20 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
 ?>
+<?$APPLICATION->IncludeComponent(
+    "bitrix:menu",
+    "left_menu",
+    Array(
+        "ALLOW_MULTI_SELECT" => "N",
+        "CHILD_MENU_TYPE" => "left",
+        "DELAY" => "N",
+        "MAX_LEVEL" => "2",
+        "MENU_CACHE_GET_VARS" => array(""),
+        "MENU_CACHE_TIME" => "3600",
+        "MENU_CACHE_TYPE" => "N",
+        "MENU_CACHE_USE_GROUPS" => "Y",
+        "ROOT_MENU_TYPE" => "left",
+        "USE_EXT" => "N"
+    )
+);?><br>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

25c81f7b022fdb97dfa225ed511466d3e0cf569c
gktr74 <gktr74@mail.ru>
2022-05-22 08:20:56 +0500

    22.05 win

---
+++ company/test/.podmenu.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Пункт1", 
+		"/company/test/p1.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Пункт 2", 
+		"/company/test/p2.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>

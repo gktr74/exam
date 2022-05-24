de1f492691ddfeac8d6135674b6e282397267ab6
gktr74 <gktr74@mail.ru>
2022-05-24 20:02:28 +0500

    24.05

--- new.php
+++ new.php
@@ -1,5 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Мебельная компания");
-?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
